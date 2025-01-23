f503cd82126547ad8421b2dbb4b9ea21925252fe
ManoharJunga <95352064+ManoharJunga@users.noreply.github.com>
2025-01-23 21:27:26 +0530

    documents page update

--- chat-with-pdf-challenge/firebaseAdmin.ts
+++ chat-with-pdf-challenge/firebaseAdmin.ts
@@ -1,6 +1,6 @@
 import { initializeApp, getApps, getApp, App, cert } from "firebase-admin/app";
 import {getFirestore} from "firebase-admin/firestore";
-
+import { getStorage } from "firebase-admin/storage";
 
 const serviceKey = require("@/service_key.json");
 
@@ -15,4 +15,6 @@ if(getApps().length === 0){
 }
 
 const adminDb = getFirestore(app);
-export { app as adminApp, adminDb};
+const adminStorage = getStorage(app);
+
+export { app as adminApp, adminDb, adminStorage};
